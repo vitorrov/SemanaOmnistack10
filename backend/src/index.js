c02c130a2f63d369677a3c62c4e0f68333fc8486
Vitor <vitormr45@gmail.com>
2020-01-16 17:37:45 -0300

    Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes.js')
+const cors = require('cors');
+const routes = require('./routes.js');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:teste123@omnistack9-ii0ol.mongodb.net/
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
