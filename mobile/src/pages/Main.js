16af1a8742cde7a53146e2b98f270abab0e9a782
Vitor <vitormr45@gmail.com>
2020-01-17 03:16:55 -0300

    Mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -36,6 +37,7 @@ function Main({ navigation }) {
     }
 
     return (
+        <>
     <MapView initialRegion={currentRegion} style={styles.map}>
         <Marker coordinate={{ latitude: -22.9887327, longitude:  -46.9851506}} >
             <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/57811481?s=460&v=4' }} />
@@ -51,6 +53,21 @@ function Main({ navigation }) {
             </Callout>
         </Marker>
         </MapView>
+
+        <View style={styles.searchForm}>
+            <TextInput 
+            style={styles.searchInput}
+            placeholder="Buscar devs por techs"
+            placeholderTextColor="#999"
+            autoCapitalize="words"
+            autoCorrect={false} 
+             />
+
+            <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                <MaterialIcons name="my-location" size={20} color="#FFF" />
+            </TouchableOpacity>
+        </View>
+        </>
     );
 }
 
@@ -83,6 +100,36 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5,
+    },
+
+    searchForm: {
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+
     },
 })
 
