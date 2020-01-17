8575c70a91dcfd3e9da104ab0f257d8e5a78d1e6
Vitor <vitormr45@gmail.com>
2020-01-17 03:03:34 -0300

    Mobile

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
+function Main({ navigation }) {
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+                const location = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const coords = location.coords;
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                })
+
+            }
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    if (!currentRegion) {
+        return null;
+    }
+
+    return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -22.9887327, longitude:  -46.9851506}} >
+            <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/57811481?s=460&v=4' }} />
+
+            <Callout onPress={() => {
+                navigation.navigate('Profile', { github_username: 'vitorrov' });
+            }}>
+                <View style={styles.callout}>
+                    <Text style={styles.devName}>Vitor</Text>
+                    <Text style={styles.devBio}>Minha bio</Text>
+                    <Text styles={styles.devTechs}>ReactJS, React Native, NodeJS</Text>
+                </View>
+            </Callout>
+        </Marker>
+        </MapView>
+    );
+}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
+    },
+
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5,
+    },
+})
+
+export default Main;
