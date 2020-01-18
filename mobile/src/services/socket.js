ca62d4d9864257464a656ffa6cc235a54d864b95
Vitor <vitormr45@gmail.com>
2020-01-18 19:56:54 -0300

    Mobile

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -4,6 +4,10 @@ const socket = socketio('http://192.168.15.29:3333', {
     autoConnect: false,
 });
 
+function subscribeToNewDevs(subscribeFunction) {
+    socket.on('new-dev', subscribeFunction);
+}
+
 function connect(latitude, longitude, techs) {
     socket.io.opts.query = {
         latitude,
@@ -27,4 +31,5 @@ function disconnect() {
 export {
     connect,
     disconnect,
+    subscribeToNewDevs,
 };
